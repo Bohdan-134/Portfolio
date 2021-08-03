494f81cd0b80ed29d43c87ee54603d296309d52e
Bohdan-134 <bkachmar1@gmail.com>
2021-08-03 22:30:32 +0300

    aboutMe

--- js/script.js
+++ js/script.js
@@ -34,3 +34,47 @@ navBtn[2].onclick = () => {
     indicator.style.top = "66.8%";
     indicator.style.transform = "none";
 }
+
+/* button cert */
+const certificateContainer = document.querySelector('.about-me__certificate');
+const certificateBtn = document.querySelector('.button-certificate');
+
+
+certificateBtn.onclick = () => {
+    if (certificateContainer.classList.contains('certificate-active')) {
+        certificateContainer.classList.remove('certificate-active');
+    } else {
+        certificateContainer.classList.add('certificate-active');
+    }
+}
+
+certificateBtn.onmouseenter = () => certificateContainer.style.top = "-15%";
+certificateBtn.onmouseleave = () => certificateContainer.style.top = "-16%";
+
+// h2
+
+const h2Word = document.querySelectorAll('.word-h2');
+
+for (let i = 0; i < h2Word.length; i++) {
+    h2Word[i].onmouseenter = () => {
+        h2Word[i].classList.add('active-h2');
+    }
+    h2Word[i].onmouseleave = () => {
+        h2Word[i].classList.remove('active-h2');
+    }
+}
+
+//text 
+
+const t1 = document.querySelectorAll('.t1');
+
+for (let i = 0; i < t1.length; i++) {
+    t1[i].onmouseenter = () => {
+        t1[i].classList.add('active-t1');
+    }
+    t1[i].onmouseleave = () => {
+        t1[i].classList.remove('active-t1');
+    }
+}
+
+//animation block
