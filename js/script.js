c1a7b6a3a411c2914ececb98c502cb2b2e514a08
Bohdan-134 <bkachmar1@gmail.com>
2021-08-03 16:43:27 +0300

    navigation3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
-$('.slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    centerMode: true,
-    centerPadding: '60px',
-    arrows: true,
+const navBtn = document.querySelectorAll('.nav-button');
+const block = document.querySelectorAll('.block');
+
+navBtn.forEach(function(item) {
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+        navBtn.forEach(function(item) {
+            item.classList.remove('active');
+        });
+        block.forEach(function(item) {
+            item.classList.remove('block-active');
+        });
+        currentBtn.classList.add('active');
+        currentTab.classList.add('block-active');
+    })
 });
+
+const indicator = document.querySelector('.indicator');
+
+
+navBtn[0].onclick = () => {
+    indicator.style.top = "0%";
+    indicator.style.transform = "none";
+}
+
+navBtn[1].onclick = () => {
+    indicator.style.top = "50%";
+    indicator.style.transform = "translateY(-50%)";
+}
+
+navBtn[2].onclick = () => {
+    indicator.style.top = "66.8%";
+    indicator.style.transform = "none";
+}
