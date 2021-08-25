2f08ab8c63384e7251191ffa472a3a7b3f5255b4
Bogdan <bkachmar1@gmail.com>
2021-08-25 18:56:38 +0300

    bodyVH

--- js/script.js
+++ js/script.js
@@ -180,6 +180,13 @@ mBtn.onclick = function openMenu() {
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+
+
 
 /* contact-me-card */
 
