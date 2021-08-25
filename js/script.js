e9c77ced2faa0b7defc48094fbbc6a1fb78388c8
Bogdan <bkachmar1@gmail.com>
2021-08-26 00:59:53 +0300

    mobile11

--- js/script.js
+++ js/script.js
@@ -198,7 +198,10 @@ const contactMeInfoBlock = document.querySelector('.contact-me__info-block');
 contactInfoBtn.onclick = () => {
     if (contactMeInfoBlock.classList.contains('open-i-contact')) {
         contactMeInfoBlock.classList.remove('open-i-contact');
+        contactInfoBtn.classList.remove('colorI');
     } else {
         contactMeInfoBlock.classList.add('open-i-contact');
+        contactMeInfoBlock.style.color = "#c06048";
+        contactInfoBtn.classList.add('colorI');
     }
 }
